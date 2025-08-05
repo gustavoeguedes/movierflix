16ddd0b3ed0ec32a3d0c6cb24b1083a9a107924e
gustavoeguedes <gustavoestevesguedes12@gmail.com>
2025-08-05 20:26:29 -0300

    feat: init setub basic

---
+++ src/main/java/br/com/movierflix/MovierflixApplication.java
@@ -0,0 +1,13 @@
+package br.com.movierflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovierflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovierflixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/movierflix/MovierflixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.movierflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovierflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
